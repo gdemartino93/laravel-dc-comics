71422cd2ed1b2d6f0f9631cc6cbec5729e82c37f
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-06 20:42:14 +0100

    some style and home icon in header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <h1>Lista persone</h1>
+<div class="container">
+    <div class="d-flex align-items-center">
+        <div class="col-4">
+            <a href="{{route('person.home')}}">
+                <i class="fa-solid fa-house"></i>
+            </a>
+        </div>
+        <div class="col-8">
+            <h1>Lista persone</h1>
+        </div>
+    </div>
 </div>
--- resources/views/pages/single.blade.php
+++ resources/views/pages/single.blade.php
@@ -5,9 +5,16 @@
 @endsection 
 
 @section('contents')
-<div class="d-flex flex-column myItem">
+<div class="row d-flex justify-content-center">
+    <div class="d-flex flex-column myItemSingle col-6">
         <h4>{{$person -> firstName}} {{$person -> lastName}}</h4>
         <span>{{$person -> dateOfBirth}}</span>
         <span>{{$person -> height}} cm </span>
+        <div>
+            <i class="fa-solid fa-user-pen"></i>
+            <i class="fa-solid fa-trash"></i>
+        </div>
+    </div>
 </div>
+
 @endsection
