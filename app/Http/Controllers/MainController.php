cc24b2ea69c46aa799b6afd36ced834332dfa083
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-06 18:54:56 +0100

    created home route

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Person;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function home(){
+        
+        $peoples = Person::all();
+
+        $data = ["peoples" => $peoples];
+        return view('pages.home',$data);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/', [MainController::class, 'home'])
+     ->name('person.home');
