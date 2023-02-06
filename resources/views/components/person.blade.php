8c0997c5e4a892a0862b3a6df5f66e8ecf91e836
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-06 18:55:11 +0100

    create component person and iterate with foreach

---
+++ resources/views/components/person.blade.php
@@ -0,0 +1,5 @@
+<div class="d-flex flex-column">
+    <h4>Nome Cognome</h4>
+    <span>Data di nascita</span>
+    <span>Altezza</span>
+</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,5 +5,11 @@
 @endsection
 
 @section('contents')
-        
+    <div class="wrapper d-flex">
+        <div class="container">
+            @foreach ($peoples as $person)
+                @include('components.person')
+            @endforeach 
+        </div>
+    </div>
 @endsection
